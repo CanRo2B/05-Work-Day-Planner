07b19d4c9a4b339f0befec5df41b1a293cef2297
Candace Robbins <canrob0522@gmail.com>
2022-04-08 08:49:56 -0500

    Fixed conditional

--- Assets/script.js
+++ Assets/script.js
@@ -6,11 +6,7 @@ var currentHr = moment();
 $("#currentDay").text(currentTime.format("dddd, MMM Do, YYYY"));
 $("#currentHour").text(currentHr.format("HH:mm"));
 
-
-var hrArray = JSON.parse(localStorage.getItem("task"))
 var task= $("task");
-
-// localStorage.getItem("tasks", JSON.parse());
 
 // Begin a function focused on entire document
 $(document).ready(function(){
@@ -19,7 +15,6 @@ $(".saveBtn").on("click", function() {
     var value = $(this).siblings(".task").val();
     var key = $(this).parent().attr("id");
     
-   
     console.log(key, value);
 
     localStorage.setItem(key, value);
@@ -28,34 +23,33 @@ $(".saveBtn").on("click", function() {
 
 
 
-
+// Function to animate the planner
 function animateTask(){
 var currentHour = parseInt(moment().format("H"));
-// Loop over timeblocks
+// Taking the time block to get the attributes and then use them for the conditional
 $(".time-block").each(function() {
 // create function to loop over time blocks
 var timeBlock = parseInt($(this).attr("id"));
 
 
-// conditional statement to check the hours and apply correct time
+// conditional statement to check the hours and apply correct time animations
     if(timeBlock < currentHour) {
         $(".task").addClass("past");
      } else 
         if (timeBlock === currentHour) {
+            $(".task").removeClass("past");
             $(".task").addClass("present");
      } else 
            {
+            $(".task").removeClass("past");
+            $(".task").removeClass("present");
             $(".task").addClass("future");
          } 
      })
 }
- 
 
-animateTask();
 
-var interval = setInterval(animateTask, 15000);
-
-// $("").val(localStorage.getItem(""));
+// Getting the planner task 
 $("#9 .task").val(localStorage.getItem("9"));
 $("#10 .task").val(localStorage.getItem("10"));
 $("#11 .task").val(localStorage.getItem("11"));
@@ -66,17 +60,10 @@ $("#15 .task").val(localStorage.getItem("15"));
 $("#16 .task").val(localStorage.getItem("16"));
 $("#17 .task").val(localStorage.getItem("17"));
 
-// $("#0").children(".task").val(localStorage.getItem("0"));
-// $("#1").children(".task").val(localStorage.getItem("1"));
-// $("#2").children(".task").val(localStorage.getItem("2"));
-// $("#3").children(".task").val(localStorage.getItem("3"));
-// $("#4").children(".task").val(localStorage.getItem("4"));
-// $("#5").children(".task").val(localStorage.getItem("5"));
-// $("#6").children(".task").val(localStorage.getItem("6"));
-// $("#7").children(".task").val(localStorage.getItem("7"));
-// $("#8").children(".task").val(localStorage.getItem("8"));
-
+animateTask();
 
 $("#currentDay").text(moment().format("MMM Do YYYY"));
  
 });
+
+// var interval = setInterval(animateTask, 15000);
